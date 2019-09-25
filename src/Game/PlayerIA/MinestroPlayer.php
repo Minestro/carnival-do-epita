a03ce1ed72d1b24a908883a1adbf1dc013af6e73
Alexandre Jacquot <alexandre.jacquot@epita.fr>
2019-09-25 21:35:59 +0200

    Nouvel algo

--- src/Game/PlayerIA/MinestroPlayer.php
+++ src/Game/PlayerIA/MinestroPlayer.php
@@ -9,6 +9,9 @@ class MinestroPlayer extends Player
     protected $mySide;
     protected $opponentSide;
     protected $result;
+    private $historicType;
+    private $historicScore;
+    private $round = 0;
 
     public function getChoice()
     {
@@ -22,9 +25,52 @@ class MinestroPlayer extends Player
         $nbTypes['paper'] = $stats[$this->opponentSide]['paper'];
         $nbTypes['rock'] = $stats[$this->opponentSide]['rock'];
         $nbTypes['scissors'] = $stats[$this->opponentSide]['scissors'];
+        $this->historicType[] = $nbTypes;
+        $this->historicScore[] = array('a' => $stats['a']['score'], 'b' => $stats['b']['score']);
 
-        $minType = array_keys($nbTypes, min($nbTypes));
+        $nbEvenGames = $this->getNbEvenGames();
+        $diffTypes = $this->getDiffTypes(3);
+        $lastPlayed = $this->getLastPlayed();
+
+        if ($nbEvenGames >= 3) {
+            $diffTypes[$lastPlayed] -= 1000;
+        }
+
+        $minType = array_keys($diffTypes, min($diffTypes));
+        $this->round++;
         return $this->getChoiceFromString($minType[0]);
+    }
+
+    private function getLastPlayed() {
+        $diff = $this->getDiffTypes(1);
+        return array_keys($diff, max($diff))[0];
+    }
+
+    private function getDiffTypes($delta) {
+        if ($delta > $this->round) {
+            return $this->historicType[$this->round];
+        }
+
+        $diff = array(
+            'rock' => $this->historicType[$this->round]['rock'] - $this->historicType[$this->round - $delta]['rock'],
+            'paper' => $this->historicType[$this->round]['paper'] - $this->historicType[$this->round - $delta]['paper'],
+            'scissors' => $this->historicType[$this->round]['scissors'] - $this->historicType[$this->round - $delta]['scissors']
+        );
+
+        return $diff;
+    }
+
+    private function getNbEvenGames() {
+        $count = 0;
+        for ($i = $this->round; $i >=1; $i--){
+            if ($this->historicScore[$i] == $this->historicScore[$i-1]) {
+                $count++;
+            } else {
+                break;
+            }
+        }
+
+        return $count;
     }
 
     private function getChoiceFromString($string) {
