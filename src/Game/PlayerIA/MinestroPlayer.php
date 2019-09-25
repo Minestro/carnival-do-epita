65f44b99c7901f865fbab3dfd84c5a98db60573c
Alexandre Jacquot <alexandre.jacquot@epita.fr>
2019-09-25 20:28:50 +0200

    Un algo de qualité supérieure

--- src/Game/PlayerIA/MinestroPlayer.php
+++ src/Game/PlayerIA/MinestroPlayer.php
@@ -12,6 +12,18 @@ class MinestroPlayer extends Player
 
     public function getChoice()
     {
-        return parent::scissorsChoice();
+        $nbTypes = array(
+            'nbPaper' => 0,
+            'nbRock' => 0,
+            'nbScissors' => 0
+        );
+
+        $stats = $this->result->getStats();
+        $nbTypes['paper'] = $stats[$this->opponentSide]['paper'];
+        $nbTypes['rock'] = $stats[$this->opponentSide]['rock'];
+        $nbTypes['scissors'] = $stats[$this->opponentSide]['scissors'];
+
+        $minType = array_keys($nbTypes, min($nbTypes));
+        return $minType[0];
     }
 }
